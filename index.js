7d20c448d9852a4db1603d2fc764a21f322466b8
Uche Godswill <auchjee@gmail.com>
2025-02-27 14:58:53 +0100

    Update UX

--- index.js
+++ index.js
@@ -7,18 +7,26 @@ function update() {
     let error = document.getElementById("error");
     error.innerHTML = "Input complete entry data before adding";
     error.style.color = "red";
-    setTimeout(()=>{error.style.display = "none"}, 3000);
+    setTimeout(() => {
+      error.style.display = "none";
+    }, 3000);
     return false;
   } else {
     usersalary = Number(usersalary).toLocaleString();
     let tr = document.createElement("tr");
-    let tbody = document.getElementById("table-body");
-    tr.innerHTML = `<td>${username}</td><td>₦${usersalary}</td><td>${date}</td>`;
+    tr.innerHTML = `<td contenteditable="true">${username}</td><td contenteditable="true">₦${usersalary}</td><td contenteditable="true">${date}</td><button onclick="del(this)"><i class="fas fa-trash-alt"></i></button>`;
     tbody.appendChild(tr);
 
     document.getElementById("name").value = "";
     document.getElementById("salary").value = "";
   }
 }
+
 let amount = 2500000;
 document.getElementById("ceo-amount").innerHTML = `₦${amount.toLocaleString()}`;
+let tbody = document.getElementById("table-body");
+function del(button){
+  let tr = button.closest("tr");
+  tr.remove();
+}
+
